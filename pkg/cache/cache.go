10f275ca297a44ecee0ab5efdd7f173a9c0f8e46
stone <xw2024newlife@outlook.com>
2024-08-29 16:54:22 +0400

    修改缓存配置

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -12,4 +12,16 @@ type Cache interface {
 	SetAndExpire(key string, value string, expire time.Duration) error
 	// 获取key对应的值
 	Get(key string) (string, error)
+
+	// LPUSH 向List添加元素
+	LPUSH(key string, values ...interface{}) (int64, error)
+
+	// LPop  获取List的第一个元素
+	LPop(key string) (string, error)
+
+	// SAdd 向Set添加元素
+	SAdd(key string, members ...interface{}) error
+
+	// SMembers 获取Set中的所有元素
+	SMembers(key string) ([]string, error)
 }
